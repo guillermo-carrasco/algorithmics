739a4617a5a30cdebffa93a82ed39f5f0555b963
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-08-28 11:13:18 +0200

    Problem 3 solved

---
+++ 3_largest_prime_factor.c
@@ -0,0 +1,52 @@
+//Problem 3 --> https://projecteuler.net/problem=3
+
+#include <stdio.h>
+#include <stdlib.h>
+
+long N=600851475143;
+
+struct node {
+	struct node* next;
+	int val;
+};
+
+void next_prime(struct node *primes, int n) {
+	char found = 0;
+	struct node *aux = primes;
+	while (!found) {
+		if (n%aux->val == 0) {
+			n++;
+			aux = primes;
+		}
+		else {
+			if (aux->next == NULL) {
+				found = 1;
+				aux->next = (struct node*) malloc(sizeof(struct node));
+				aux->next->val = n;
+				aux->next->next = NULL;
+			}
+			else (aux = aux->next);
+		}
+	}
+}
+
+int main() {
+	/* You can't malloc outside the main (or any function). Malloc allocates memory
+	 on the heap, i.e runtime, so you can't allocate on compile time. You can
+	 though declare the variable and it will be saved in the program page on
+	 compile time */
+	struct node *primes = (struct node*) malloc(sizeof(struct node));
+	primes->val = 2;
+	primes->next = NULL;
+	while (N > 1) {
+		while (N%primes->val == 0) {
+			N /= primes->val;
+		}
+		if (N>1) {
+			next_prime(primes, primes->val);
+			primes = primes->next;
+		}
+	}
+	printf("%d\n", primes->val);
+	return 0;
+}
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 https://projecteuler.net
 
 1. [Problem 1](https://projecteuler.net/problem=1) - 1_mult_3_5.c
-2. [Problem 2](https://projecteuler.net/problem=1) - 2_even_fibonacci.c
+2. [Problem 2](https://projecteuler.net/problem=2) - 2_even_fibonacci.c
+3. [Problem 3](https://projecteuler.net/problem=3) - 3_largest_prime_project.c
