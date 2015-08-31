45d0726d7594e4bbae8acd63ad323715f3336e54
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-08-31 10:35:25 +0200

    Added explanation on smarter solution

--- src/smallest_multiple.c
+++ src/smallest_multiple.c
@@ -1,3 +1,21 @@
+/*
+ * This is the bruteforce solution. A smarter solution would be to do a prime 
+ * factorization of 1..N and then get the product of the highest powers. This
+ * takes advantage of the lowest common multiple. i.e for 1..10:
+ *
+ * 10 = 5 * 2
+ * 9 = 3^2
+ * 8 = 2^3
+ * 7 = 7
+ * 6 = 3 * 2
+ * 5 = 5
+ * 4 = 2^2
+ * 3 = 3
+ * 2 = 2
+ *
+ * res = 5 * 3^2 * 2^3 * 7 = 2520
+ * */
+
 #include <stdio.h>
 #include <stdlib.h>
 
